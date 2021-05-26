ecc945d9635e2f505dad7b127ed42b275181e328
jmcia2020 <jeanmmcmahon@gmail.com>
2021-05-26 17:50:28 -0500

    nav bar modified. Access to rest of page.

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,21 +1,43 @@
 import { NavLink } from 'react-router-dom';
-import NavLogin from './NavLogin';
-//import Navbar from 'react-bootstrap/Navbar'
+//import NavLogin from './NavLogin';
+import { Nav, Navbar } from 'react-bootstrap'
 
 
 export default function NavBar(){
 
 
   return (
-    <>
-    <ul>
-      <li><NavLink to="/" >Home</NavLink></li>
-      <li><NavLink to="/Art">Art</NavLink></li>
-      <li><NavLink to="/CreateArt">Create Art</NavLink></li>
-      <li><NavLink to="/Collections">Collections</NavLink></li>
-      <li><NavLink to="/Profile">Profile</NavLink></li>
-      <li><NavLogin/></li>
-    </ul>
-    </>
-  )
-}
+      <>
+        <Navbar class="navbar navbar-expand-sm navbar-dark bg-dark">
+          <Nav className="mr-auto" variant="tabs">
+          
+          <Nav.Item>
+            <NavLink to="/">Home</NavLink>
+          </Nav.Item>
+
+          <Nav.Item>
+            <NavLink to="/Art">Art</NavLink>
+          </Nav.Item>
+
+          <Nav.Item>
+            <NavLink to="/CreateArt">Create Art</NavLink>
+          </Nav.Item>
+
+          <Nav.Item>
+            <NavLink to="/Collections">Collections</NavLink>
+          </Nav.Item>
+
+          <Nav.Item>
+            <NavLink to="/Profile">Profile</NavLink>
+          </Nav.Item>
+          
+          <Nav.Item>
+            <NavLink to="/NavLogin">Login</NavLink>
+          </Nav.Item>
+            
+          </Nav>
+        </Navbar>
+      </>
+      
+    )
+  }
