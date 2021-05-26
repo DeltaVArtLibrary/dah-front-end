6e7a7b72298f936c34de584e4aff63a35c2b1572
Vavyo <vavyo2121@gmail.com>
2021-05-26 10:59:32 -0500

    Added collection cards with accordians

--- src/Collections.js
+++ src/Collections.js
@@ -1,19 +1,48 @@
 import { useEffect, useState } from 'react';
+import useFetch from './hooks/useFetch';
+import CollectionCard from './Components/collections/CollectionCard';
 
-export default function Collections(url) {
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    async function doFetch() {
-      console.log('fetching');
-      let response = await fetch(url);
-      let body = await response.json();
-      setData(body);
-    }
-    doFetch();
-  }, [url]);
+export default function Collections() {
+  // const [data, setData] = useState(null);
+  // useEffect(() => {
+  //   async function doFetch() {
+  //     console.log('fetching');
+  //     let response = await fetch(url);
+  //     let body = await response.json();
+  //     setData(body);
+  //   }
+  //   doFetch();
+  // }, [url]);
+
+  const { data } =  useFetch('https://digitalarthub.azurewebsites.net/api/Collections');
+
+  console.log(data);
+
+
   return (
-    data,
-    <h1>Welcome to the Collections page</h1>
-    
+    <>
+      <h1>Welcome to the art page.</h1>
+      <div>
+       <CollectionList collections={data}/>
+      </div>
+    </>
   );
+
+}
+
+function CollectionList(props){
+  var collectionsArray=props.collections;
+  console.log(props);
+  if(!collectionsArray){
+    return (<div>
+      Loading...
+    </div>);
+  }
+  return(
+    <div>
+      {collectionsArray.map((collection) => 
+      CollectionCard(collection)
+        )}
+    </div>
+  )
 }
---
+++ src/Components/collections/CollectionCard.js
@@ -0,0 +1,31 @@
+import { useFetch } from '../../hooks/useFetch';
+import { Card, Button, Accordion } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+
+export default function CollectionCard(data) {
+  
+  
+  return(
+    <Card className='Collection-Card'>
+      <Card.Body>
+        <Card.Title>{data.title}</Card.Title>
+        {/* <Card.Subtitle>{data.profileDisplayName}</Card.Subtitle> */}
+        <Card.Text>
+          {data.description}
+        </Card.Text>
+        <Accordion>
+          <Accordion.Toggle as={Button} variant={data.art.length?"primary":"secondary"} eventKey={data.collectionId} disabled={data.art.length===0}>View: {data.art.length}</Accordion.Toggle>
+          <Accordion.Collapse eventKey={data.collectionId}>
+            <div>
+            {data.art.map((art) => 
+              <div>{art.title}</div>
+            )}
+            </div>
+          </Accordion.Collapse>
+        </Accordion>
+            
+        
+      </Card.Body>
+    </Card>
+  )
+}
