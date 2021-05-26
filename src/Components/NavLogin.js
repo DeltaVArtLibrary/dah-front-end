63848bd16711ba099d25f45881cde514d4d0df91
adard2002 <adard2002@gmail.com>
2021-05-26 10:10:42 -0500

    able to login and logout

--- src/Components/NavLogin.js
+++ src/Components/NavLogin.js
@@ -1,11 +1,24 @@
 import Login from './auth/Login';
 import Register from './auth/Register';
 import { useState } from 'react';
+import { useAuth } from '../Context/auth';
 
 
 
 export default function NavLogin(){
   const [showForm, setShowForm] = useState();
+
+  const { user, logout } = useAuth();
+
+  if (user) {
+      function handleLogout() {
+          logout();
+      }
+
+      return (
+          <button className="logout" onClick={handleLogout}>Log Out</button>
+      )
+  }
 
   return (
     <>
